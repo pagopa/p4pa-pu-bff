2dc2a09400b6848266fc24e6c7b97d3d54c2ffc8
antonio.torre <antonio.torre@pagopa.it>
2024-12-03 19:34:23 +0100

    P4ADEV-1557 init-repo

--- src/main/java/it/gov/pagopa/pu/bff/PiattaformaUnitariaBffApplication.java
+++ src/main/java/it/gov/pagopa/pu/bff/PiattaformaUnitariaBffApplication.java
@@ -1,13 +1,13 @@
-package it.gov.pagopa.template;
+package it.gov.pagopa.pu.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class PiattaformaUnitariaBffApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateApplication.class, args);
+		SpringApplication.run(PiattaformaUnitariaBffApplication.class, args);
 	}
 
 }

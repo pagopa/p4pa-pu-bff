6e0f114c057938793ba67246e25c8c685b4a93b8
ElisKina-dev <elis.kina@nttdata.com>
2024-12-13 12:48:45 +0100

    test getOrganizations

---
+++ src/test/java/it/gov/pagopa/pu/bff/controller/OrganizationControllerTest.java
@@ -0,0 +1,64 @@
+package it.gov.pagopa.pu.bff.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.gov.pagopa.pu.bff.dto.generated.OrganizationDTO;
+import it.gov.pagopa.pu.bff.security.JwtAuthenticationFilter;
+import it.gov.pagopa.pu.bff.service.organization.OrganizationServiceImpl;
+import it.gov.pagopa.pu.bff.util.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(value = OrganizationController.class, excludeFilters = @ComponentScan.Filter(
+  type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class))
+@AutoConfigureMockMvc(addFilters = false)
+class OrganizationControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockBean
+  private OrganizationServiceImpl organizationServiceMock;
+
+  private List<OrganizationDTO> organizationDTOList;
+
+  @BeforeEach
+  void setUp() {
+    organizationDTOList = new ArrayList<>();
+  }
+
+  @Test
+  void testGetOrganizations() throws Exception {
+    TestUtils.addSampleUserIntoSecurityContext();
+
+    Mockito.when(organizationServiceMock.getOrganizations(TestUtils.getSampleUser(), "fakeAccessToken"))
+      .thenReturn(organizationDTOList);
+
+    MvcResult result = mockMvc.perform(get("/bff/organizations")
+        .contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andReturn();
+
+    Assertions.assertEquals(result.getResponse().getContentAsString(), objectMapper.writeValueAsString(organizationDTOList));
+  }
+
+}
---
+++ src/test/java/it/gov/pagopa/pu/bff/util/TestUtils.java
@@ -0,0 +1,71 @@
+package it.gov.pagopa.pu.bff.util;
+
+import it.gov.pagopa.pu.p4paauth.model.generated.UserInfo;
+import it.gov.pagopa.pu.p4paauth.model.generated.UserOrganizationRoles;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class TestUtils {
+
+  private TestUtils(){}
+
+  /**
+   * It will assert not null on all o's fields
+   */
+  public static void checkNotNullFields(Object o, String... excludedFields) {
+    Set<String> excludedFieldsSet = new HashSet<>(Arrays.asList(excludedFields));
+    org.springframework.util.ReflectionUtils.doWithFields(o.getClass(),
+      f -> {
+        f.setAccessible(true);
+        Assertions.assertNotNull(f.get(o), "The field "+f.getName()+" of the input object of type "+o.getClass()+" is null!");
+      },
+      f -> !excludedFieldsSet.contains(f.getName()));
+  }
+
+  public static void addSampleUserIntoSecurityContext(){
+    UserInfo userInfo = getSampleUser();
+
+    Collection<? extends GrantedAuthority> authorities = null;
+    if (userInfo.getOrganizationAccess() != null) {
+      authorities = userInfo.getOrganizations().stream()
+        .filter(o -> userInfo.getOrganizationAccess().equals(o.getOrganizationIpaCode()))
+        .flatMap(r -> r.getRoles().stream())
+        .map(SimpleGrantedAuthority::new)
+        .toList();
+    }
+    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userInfo, "token", authorities);
+    SecurityContextHolder.getContext().setAuthentication(authToken);
+  }
+
+  public static UserInfo getSampleUser(){
+    List<UserOrganizationRoles> organizations = List.of(
+      new UserOrganizationRoles()
+        .operatorId("operator1")
+        .organizationIpaCode("ORG")
+        .email("email1@example.com")
+        .roles(List.of("ROLE")),
+      new UserOrganizationRoles()
+        .operatorId("operator2")
+        .organizationIpaCode("ORG2")
+        .email("email2@example.com")
+        .roles(List.of("ROLE2"))
+    );
+    return new UserInfo().mappedExternalUserId("MAPPEDEXTERNALUSERID")
+      .fiscalCode("FISCALCODE")
+      .familyName("FAMILYNAME")
+      .name("NAME")
+      .issuer("ISSUER")
+      .organizationAccess("ORG")
+      .organizations(organizations);
+  }
+
+
+}

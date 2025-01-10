f076f1ec41d48b6709e194e379827326f01cb06b
antoniocalo <antonio.calo@nttdata.com>
2025-01-10 12:40:02 +0100

    P4ADEV-1594 add CoreHealthIndicatorServiceTest

---
+++ src/test/java/it/gov/pagopa/pu/bff/service/CoreHealthIndicatorServiceTest.java
@@ -0,0 +1,62 @@
+package it.gov.pagopa.pu.bff.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.maciejwalkowiak.wiremock.spring.ConfigureWireMock;
+import com.maciejwalkowiak.wiremock.spring.EnableWireMock;
+import com.maciejwalkowiak.wiremock.spring.InjectWireMock;
+import it.gov.pagopa.pu.bff.config.MonitoringServiceConf;
+import it.gov.pagopa.pu.bff.dto.generated.ServiceStatus;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@SpringBootTest(
+  classes = {CoreHealthIndicatorService.class, CoreHealthIndicatorServiceTest.CoreHealthIndicatorServiceTestConfiguration.class},
+  webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@EnableWireMock({
+  @ConfigureWireMock(name = "monitoring-server", port = 8888)
+})
+class CoreHealthIndicatorServiceTest {
+
+  @InjectWireMock(value = "monitoring-server")
+  private static WireMockServer wireMockServer;
+
+  @Autowired
+  private CoreHealthIndicatorService coreHealthIndicatorService;
+
+  @MockitoBean
+  private MonitoringServiceConf monitoringServiceConf;
+
+  @TestConfiguration
+  public static class CoreHealthIndicatorServiceTestConfiguration {
+    @Bean
+    public RestTemplateBuilder restTemplateBuilder() {
+      return new RestTemplateBuilder();
+    }
+  }
+  @Test
+  void givenValidRequestWhenGetStatusThenOk() {
+    // When
+    Mockito.when(monitoringServiceConf.getServices()).thenReturn(Map.of(
+      "P4PA", new MonitoringServiceConf.ServiceConfig("P4PA", "http://localhost:8888/actuator/health/readiness")
+    ));
+
+    List<ServiceStatus> result = coreHealthIndicatorService.getStatus();
+
+    // Then
+    assertEquals(1, result.size());
+
+    ServiceStatus serviceAStatus = result.get(0);
+    assertEquals("P4PA", serviceAStatus.getServiceName());
+    assertEquals("\"UP\"", serviceAStatus.getStatus());
+  }
+}

2f8b5c67bc82cc00beacf4057a41496d85209f22
pelliccm <matteo.pelliccione@nttdata.com>
2025-02-11 11:54:01 +0100

    [P4ADEV-2045] Clear code smells

--- src/test/java/it/gov/pagopa/pu/bff/connector/process_executions/config/ProcessExecutionsApisHolderTest.java
+++ src/test/java/it/gov/pagopa/pu/bff/connector/process_executions/config/ProcessExecutionsApisHolderTest.java
@@ -9,7 +9,6 @@ import java.time.OffsetDateTime;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;

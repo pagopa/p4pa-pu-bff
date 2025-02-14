c03f538a4779ecd5f1da0084810455d0fec50447
pelliccm <matteo.pelliccione@nttdata.com>
2025-02-14 10:06:08 +0100

    [P4ADEV-2045] Clear code smells

--- src/main/java/it/gov/pagopa/pu/bff/dto/ExportFileFiltersDTO.java
+++ src/main/java/it/gov/pagopa/pu/bff/dto/ExportFileFiltersDTO.java
@@ -1,7 +1,6 @@
 package it.gov.pagopa.pu.bff.dto;
 
 import it.gov.pagopa.pu.processexecutions.dto.generated.ExportFile.FlowFileTypeEnum;
-import java.time.OffsetDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;

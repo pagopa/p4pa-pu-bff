ce65e0dee003d8de69a478b349c1e0768c8d0e5a
antonio.torre <antonio.torre@pagopa.it>
2025-02-24 15:32:01 +0100

    P4ADEV-2136 fix CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   java
-  id("org.springframework.boot") version "3.4.1"
+  id("org.springframework.boot") version "3.4.3"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
   id("org.sonarqube") version "6.0.1.5171"
@@ -29,12 +29,12 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "2.7.0"
+val springDocOpenApiVersion = "2.8.5"
 val openApiToolsVersion = "0.2.6"
-val micrometerVersion = "1.4.1"
+val micrometerVersion = "1.4.3"
 val caffeineVersion = "3.2.0"
 
-val wiremockVersion = "3.10.0"
+val wiremockVersion = "3.12.0"
 val wiremockSpringBootVersion = "2.1.3"
 val podamVersion = "8.0.2.RELEASE"
 

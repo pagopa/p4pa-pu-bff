e3500fe5a0e8a6b033172fa03e039fc73d12af1c
antonio.torre <antonio.torre@pagopa.it>
2025-03-28 12:41:39 +0100

    fix: OpenApiGeneratorTest logging changes observed on OpenApi

--- src/test/java/it/gov/pagopa/pu/bff/OpenApiGeneratorTest.java
+++ src/test/java/it/gov/pagopa/pu/bff/OpenApiGeneratorTest.java
@@ -6,6 +6,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,7 @@ import org.springframework.test.web.servlet.MvcResult;
 @TestPropertySource(properties = {
   "logging.level.org.springdoc.core.utils.SpringDocAnnotationsUtils=OFF"
 })
+@Slf4j
 class OpenApiGeneratorTest {
 
   @Autowired
@@ -52,7 +55,7 @@ class OpenApiGeneratorTest {
         JsonAssert.comparator(JsonCompareMode.STRICT).assertIsMatch(storedOpenApi, openApiResult);
         toStore=false;
       } catch (Throwable e){
-        //Do Nothing
+        log.info("Observed the following changes: {}", e.getMessage());
       }
     }
     if(toStore){

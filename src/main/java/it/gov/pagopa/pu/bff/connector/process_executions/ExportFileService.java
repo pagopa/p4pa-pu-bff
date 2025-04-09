3ef1b892357f2a206a34f2b5e984b6f8b2acea9d
mscarsel <marco.scarsella@nttdata.com>
2025-04-09 14:47:19 +0200

    [P4ADEV-2624] divided createExportFile in multiple APIs

--- src/main/java/it/gov/pagopa/pu/bff/connector/process_executions/ExportFileService.java
+++ src/main/java/it/gov/pagopa/pu/bff/connector/process_executions/ExportFileService.java
@@ -1,8 +1,10 @@
 package it.gov.pagopa.pu.bff.connector.process_executions;
 
 import it.gov.pagopa.pu.bff.dto.ExportFileFiltersDTO;
-import it.gov.pagopa.pu.processexecutions.dto.generated.ExportFileRequestDTO;
+import it.gov.pagopa.pu.processexecutions.dto.generated.ClassificationsExportFileRequestDTO;
 import it.gov.pagopa.pu.processexecutions.dto.generated.PagedModelExportFile;
+import it.gov.pagopa.pu.processexecutions.dto.generated.PaidExportFileRequestDTO;
+import it.gov.pagopa.pu.processexecutions.dto.generated.PaymentsReportingExportFileRequestDTO;
 import org.springframework.data.domain.Pageable;
 
 public interface ExportFileService {
@@ -10,5 +12,7 @@ public interface ExportFileService {
   PagedModelExportFile getExportFiles(ExportFileFiltersDTO exportFileFilters,
     String operatorExternalId, Pageable pageable, String accessToken);
 
-  void createExportFile(ExportFileRequestDTO requestDTO, String accessToken);
+  void createPaidExportFile(PaidExportFileRequestDTO requestDTO, String accessToken);
+  void createClassificationsExportFile(ClassificationsExportFileRequestDTO requestDTO, String accessToken);
+  void createPaymentsReportingExportFile(PaymentsReportingExportFileRequestDTO requestDTO, String accessToken);
 }
--- src/main/java/it/gov/pagopa/pu/bff/service/export_flow_file/ExportFileRetrieverService.java
+++ src/main/java/it/gov/pagopa/pu/bff/service/export_flow_file/ExportFileRetrieverService.java
@@ -3,12 +3,18 @@ package it.gov.pagopa.pu.bff.service.export_flow_file;
 import it.gov.pagopa.pu.auth.dto.generated.UserInfo;
 import it.gov.pagopa.pu.bff.dto.ExportFileFiltersDTO;
 import it.gov.pagopa.pu.bff.dto.generated.PagedExportFile;
-import it.gov.pagopa.pu.processexecutions.dto.generated.ExportFileRequestDTO;
+import it.gov.pagopa.pu.processexecutions.dto.generated.ClassificationsExportFileRequestDTO;
+import it.gov.pagopa.pu.processexecutions.dto.generated.PaidExportFileRequestDTO;
+import it.gov.pagopa.pu.processexecutions.dto.generated.PaymentsReportingExportFileRequestDTO;
 import org.springframework.data.domain.Pageable;
 
 public interface ExportFileRetrieverService {
 
   PagedExportFile getExportFiles(ExportFileFiltersDTO exportFileFiltersDTO, Pageable pageable, UserInfo loggedUser, String accessToken);
 
-  void createExportFile(ExportFileRequestDTO requestDTO, UserInfo user, String accessToken);
+  void createPaidExportFile(PaidExportFileRequestDTO requestDTO, UserInfo user, String accessToken);
+  void createClassificationsExportFile(
+    ClassificationsExportFileRequestDTO requestDTO, UserInfo user, String accessToken);
+  void createPaymentsReportingExportFile(
+    PaymentsReportingExportFileRequestDTO requestDTO, UserInfo user, String accessToken);
 }

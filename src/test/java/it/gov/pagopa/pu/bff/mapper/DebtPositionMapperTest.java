3668105baae701936ed0b3d4aa8e66279fa353b1
pelliccm <matteo.pelliccione@nttdata.com>
2025-05-21 11:43:56 +0200

    [P4ADEV-2547] Sort Installments in Debt Position Detail API

--- src/test/java/it/gov/pagopa/pu/bff/mapper/DebtPositionMapperTest.java
+++ src/test/java/it/gov/pagopa/pu/bff/mapper/DebtPositionMapperTest.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.pu.bff.mapper;
 import it.gov.pagopa.pu.bff.dto.generated.DebtPositionDetailDTO;
 import it.gov.pagopa.pu.bff.util.TestUtils;
 import it.gov.pagopa.pu.debtpositions.dto.generated.*;
+import java.time.LocalDate;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,9 +27,20 @@ class DebtPositionMapperTest {
   @Test
   void givenNoMultiDebtorDebtPositionWhenMapToDebtPositionDetailDTOThenCorrectMapping() {
     PaymentOptionDTO paymentOption = podamFactory.manufacturePojo(PaymentOptionDTO.class);
+    InstallmentDTO installment1 = podamFactory.manufacturePojo(InstallmentDTO.class);
+    installment1.setDueDate(LocalDate.now().plusDays(3));
+    InstallmentDTO installment2 = podamFactory.manufacturePojo(InstallmentDTO.class);
+    installment2.setDueDate(null);
+    InstallmentDTO installment3 = podamFactory.manufacturePojo(InstallmentDTO.class);
+    installment3.setDueDate(LocalDate.now());
+    InstallmentDTO installment4 = podamFactory.manufacturePojo(InstallmentDTO.class);
+    installment4.setDueDate(LocalDate.now().minusDays(2));
+    paymentOption.setInstallments(List.of(installment1, installment2, installment3, installment4));
+
     DebtPositionDTO debtPositionDTO = podamFactory.manufacturePojo(DebtPositionDTO.class);
     debtPositionDTO.setMultiDebtor(false);
     debtPositionDTO.setPaymentOptions(List.of(paymentOption));
+
     DebtPositionTypeOrg debtPositionTypeOrg = podamFactory.manufacturePojo(DebtPositionTypeOrg.class);
 
     DebtPositionDetailDTO result = mapper.mapToDebtPositionDetailDTO(debtPositionDTO,debtPositionTypeOrg);
@@ -36,6 +48,13 @@ class DebtPositionMapperTest {
     Assertions.assertNotNull(result);
     TestUtils.checkNotNullFields(result);
     TestUtils.checkNotNullFields(result.getDebtor());
+
+    // Assert right Installments order
+    Assertions.assertEquals(installment4, result.getPaymentOptions().getFirst().getInstallments().get(0));
+    Assertions.assertEquals(installment3, result.getPaymentOptions().getFirst().getInstallments().get(1));
+    Assertions.assertEquals(installment1, result.getPaymentOptions().getFirst().getInstallments().get(2));
+    Assertions.assertEquals(installment2, result.getPaymentOptions().getFirst().getInstallments().get(3));
+
     verifyDebtPositionDetailDTO(result,debtPositionDTO,debtPositionTypeOrg);
   }
 

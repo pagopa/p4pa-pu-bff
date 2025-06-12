ecc3be49ab86540ea0d466bc0cc020c9b5f5f10b
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-06-12 11:34:23 +0200

    fix: Fix CVE (#152)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.util.*
 
 plugins {
   java
-  id("org.springframework.boot") version "3.4.5"
+  id("org.springframework.boot") version "3.5.0"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
   id("org.sonarqube") version "6.1.0.5360"
@@ -32,11 +32,11 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "2.8.6"
+val springDocOpenApiVersion = "2.8.9"
 val openApiToolsVersion = "0.2.6"
-val micrometerVersion = "1.4.6"
+val micrometerVersion = "1.5.1"
 val caffeineVersion = "3.2.0"
-val httpClientVersion = "5.4.4"
+val httpClientVersion = "5.5"
 val mapStructVersion = "1.6.3"
 val openHtmlToPdfVersion = "1.0.10"
 

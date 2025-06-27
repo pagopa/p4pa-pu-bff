432c2f1cc35224471cb13dd3b34f4fc017460731
MScarsella <marco.scarsella@emeal.nttdata.com>
2025-06-27 12:39:40 +0200

    fix: P4ADEV-3279 mapped OrgSilService to OrgSilServiceDTO (#170)
    Co-authored-by: mscarsel <marco.scarsella@nttdata.com>

--- src/main/java/it/gov/pagopa/pu/bff/service/org_sil_service/OrgSilServiceRetrieverServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/bff/service/org_sil_service/OrgSilServiceRetrieverServiceImpl.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.pu.bff.service.org_sil_service;
 
 import it.gov.pagopa.pu.auth.dto.generated.UserInfo;
 import it.gov.pagopa.pu.bff.connector.organization.OrgSilServiceService;
+import it.gov.pagopa.pu.bff.dto.OrgSilServiceDTO;
+import it.gov.pagopa.pu.bff.mapper.OrgSilServiceDTOMapper;
 import it.gov.pagopa.pu.bff.service.AuthorizationService;
 import it.gov.pagopa.pu.organization.dto.generated.CollectionModelOrgSilService;
 import it.gov.pagopa.pu.organization.dto.generated.OrgSilService;
@@ -17,19 +19,21 @@ import java.util.List;
 public class OrgSilServiceRetrieverServiceImpl implements OrgSilServiceRetrieverService {
 
   private final OrgSilServiceService orgSilServiceService;
+  private final OrgSilServiceDTOMapper orgSilServiceDTOMapper;
 
-  public OrgSilServiceRetrieverServiceImpl(OrgSilServiceService orgSilServiceService) {
+  public OrgSilServiceRetrieverServiceImpl(OrgSilServiceService orgSilServiceService, OrgSilServiceDTOMapper orgSilServiceDTOMapper) {
       this.orgSilServiceService = orgSilServiceService;
+      this.orgSilServiceDTOMapper = orgSilServiceDTOMapper;
   }
 
   @Override
-  public List<OrgSilService> getOrgSilServices(Long organizationId, OrgSilServiceType serviceType, UserInfo loggedUser, String accessToken) {
+  public List<OrgSilServiceDTO> getOrgSilServices(Long organizationId, OrgSilServiceType serviceType, UserInfo loggedUser, String accessToken) {
     AuthorizationService.validateUserForOrganizationId(organizationId,loggedUser);
     CollectionModelOrgSilService collection = orgSilServiceService.getOrgSilServices(organizationId,serviceType,accessToken);
     if (collection == null || collection.getEmbedded() == null) {
       return Collections.emptyList();
     }
-    return collection.getEmbedded().getOrgSilServices();
+    return orgSilServiceDTOMapper.map(collection.getEmbedded().getOrgSilServices());
   }
 
   @Override

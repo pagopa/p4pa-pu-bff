88628a022b695e7a218cdd59b8a5196b0ac98e45
mscarsel <marco.scarsella@nttdata.com>
2025-06-27 11:29:44 +0200

    [P4ADEV-3279] fixed mapper to use the same method for authConfig

--- src/main/java/it/gov/pagopa/pu/bff/mapper/OrgSilServiceDTOMapper.java
+++ src/main/java/it/gov/pagopa/pu/bff/mapper/OrgSilServiceDTOMapper.java
@@ -3,8 +3,7 @@ package it.gov.pagopa.pu.bff.mapper;
 import it.gov.pagopa.pu.bff.dto.OrgSilServiceDTO;
 import it.gov.pagopa.pu.organization.dto.generated.OrgSilService;
 import it.gov.pagopa.pu.organization.dto.generated.OrgSilServiceRequestBodyAuthConfig;
-import it.gov.pagopa.pu.organization.dto.generated.SilServiceLegacyBasicAuthConfig;
-import it.gov.pagopa.pu.organization.dto.generated.SilServiceLegacyJwtAuthConfig;
+import it.gov.pagopa.pu.organization.dto.generated.SilServiceAuthConfig;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
@@ -14,38 +13,16 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface OrgSilServiceDTOMapper {
   @Mapping(target = "authConfig", ignore = true)
-  @Mapping(target = "legacyBasicAuthConfig", conditionExpression = "java(orgSilService.getAuthConfig()!=null && \"legacyBasic\".equals(orgSilService.getAuthConfig().getAuthConfig()))", source = "authConfig", qualifiedByName = "castToLegacyBasic")
-  @Mapping(target = "legacyJwtAuthConfig", conditionExpression = "java(orgSilService.getAuthConfig()!=null && \"legacyJwt\".equals(orgSilService.getAuthConfig().getAuthConfig()))", source = "authConfig", qualifiedByName = "castToLegacyJwt")
+  @Mapping(target = "legacyBasicAuthConfig", conditionExpression = "java(orgSilService.getAuthConfig()!=null && \"legacyBasic\".equals(orgSilService.getAuthConfig().getAuthConfig()))", source = "authConfig", qualifiedByName = "mapAuthConfig")
+  @Mapping(target = "legacyJwtAuthConfig", conditionExpression = "java(orgSilService.getAuthConfig()!=null && \"legacyJwt\".equals(orgSilService.getAuthConfig().getAuthConfig()))", source = "authConfig", qualifiedByName = "mapAuthConfig")
   OrgSilServiceDTO map(OrgSilService orgSilService);
 
   List<OrgSilServiceDTO> map(List<OrgSilService> orgSilServiceList);
 
-  @Named("castToLegacyBasic")
-  static SilServiceLegacyBasicAuthConfig castToLegacyBasic(OrgSilServiceRequestBodyAuthConfig config) {
-    if (config instanceof SilServiceLegacyBasicAuthConfig legacy) {
-      return SilServiceLegacyBasicAuthConfig.builder()
-              .authConfig(legacy.getAuthConfig())
-              .authUrl(legacy.getAuthUrl())
-              .user(legacy.getUser())
-              .psw(legacy.getPsw())
-              .build();
-    }
-    return null;
-  }
-
-  @Named("castToLegacyJwt")
-  static SilServiceLegacyJwtAuthConfig castToLegacyJwt(OrgSilServiceRequestBodyAuthConfig config) {
-    if (config instanceof SilServiceLegacyJwtAuthConfig legacy) {
-      return SilServiceLegacyJwtAuthConfig.builder()
-              .authConfig(legacy.getAuthConfig())
-              .kid(legacy.getKid())
-              .subject(legacy.getSubject())
-              .issuer(legacy.getIssuer())
-              .algorithm(legacy.getAlgorithm())
-              .signingKey(legacy.getSigningKey())
-              .build();
-    }
-    return null;
+  @SuppressWarnings("unchecked")
+  @Named("mapAuthConfig")
+  default <T extends SilServiceAuthConfig> T mapAuthConfig(OrgSilServiceRequestBodyAuthConfig config){
+    return (T) config;
   }
 }
 

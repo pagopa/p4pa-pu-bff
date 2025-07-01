64cc3d6b7d308985ace5e5ac1ebbf1a0b779e137
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-07-01 09:23:42 +0200

    P4PADEV-2960 updated controller

--- src/main/java/it/gov/pagopa/pu/bff/exception/InvalidParameterException.java
+++
@@ -1,7 +0,0 @@
-package it.gov.pagopa.pu.bff.exception;
-
-public class InvalidParameterException extends RuntimeException {
-  public InvalidParameterException(String message) {
-    super(message);
-  }
-}

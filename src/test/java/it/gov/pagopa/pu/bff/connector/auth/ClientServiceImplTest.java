d18fa4c70d53ab65e5734b10b6c9e9a441cbf716
antoniocalo <antonio.calo@nttdata.com>
2025-07-30 08:46:58 +0200

    P4ADEV-3536 add test

---
+++ src/test/java/it/gov/pagopa/pu/bff/connector/auth/ClientServiceImplTest.java
@@ -0,0 +1,55 @@
+package it.gov.pagopa.pu.bff.connector.auth;
+
+
+import it.gov.pagopa.pu.auth.dto.generated.ClientDTOPage;
+import it.gov.pagopa.pu.auth.dto.generated.ClientNoSecretDTO;
+import it.gov.pagopa.pu.bff.connector.auth.client.AuthzClient;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+@ExtendWith(MockitoExtension.class)
+class ClientServiceImplTest {
+
+  @Mock
+  private AuthzClient authzClientMock;
+
+  private ClientService clientService;
+
+  @BeforeEach
+  void setUp() {
+    clientService = new ClientServiceImpl(authzClientMock);
+  }
+
+  @Test
+  void whenGetClientsThenSuccess(){
+    // Given
+    String accessToken = "ACCESSTOKEN";
+    Pageable pageRequest = PageRequest.of(1, 1);
+
+    ClientDTOPage expectedResult = new ClientDTOPage();
+    expectedResult.setContent(
+      List.of(ClientNoSecretDTO.builder()
+        .organizationIpaCode("IPACODE")
+        .clientId("CLIENTID")
+        .clientName("CLIENTNAME")
+        .build()));
+
+    Mockito.when(authzClientMock.getClients("IPACODE", null, null, pageRequest, accessToken))
+      .thenReturn(expectedResult);
+
+    // When
+    ClientDTOPage result = clientService.getClients("IPACODE", null, null, pageRequest, accessToken);
+
+    // Then
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(expectedResult, result);
+  }
+}
--- src/test/java/it/gov/pagopa/pu/bff/connector/auth/client/AuthzClientTest.java
+++ src/test/java/it/gov/pagopa/pu/bff/connector/auth/client/AuthzClientTest.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.Mockito.when;
 
 import it.gov.pagopa.pu.auth.controller.generated.AuthzApi;
+import it.gov.pagopa.pu.auth.dto.generated.ClientDTOPage;
 import it.gov.pagopa.pu.auth.dto.generated.OperatorsPage;
 import it.gov.pagopa.pu.auth.dto.generated.UserInfo;
 import it.gov.pagopa.pu.bff.connector.auth.config.AuthApisHolder;
+import it.gov.pagopa.pu.bff.util.PageUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +17,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
@@ -83,6 +87,25 @@ class AuthzClientTest {
       .thenReturn(expectedResult);
 
     OperatorsPage result = authzClient.getOrganizationOperators(organizationIpaCode, null, null, null, 0, 10, accessToken);
+
+    assertSame(expectedResult, result);
+  }
+
+  @Test
+  void whenGetClientsThenInvokeWithAccessToken() {
+    String accessToken = "ACCESSTOKEN";
+    String organizationIpaCode = "IPACODE";
+    ClientDTOPage expectedResult = new ClientDTOPage();
+
+    Pageable pageRequest = PageRequest.of(4, 1);
+
+    when(authApisHolderMock.getAuthzApi(accessToken))
+      .thenReturn(authzApiMock);
+    when(authzApiMock.getClientsSearch(organizationIpaCode, null, null,
+      pageRequest.getPageNumber(), pageRequest.getPageSize(), PageUtils.getSortList(pageRequest)))
+      .thenReturn(expectedResult);
+
+    ClientDTOPage result = authzClient.getClients(organizationIpaCode, null, null, pageRequest, accessToken );
 
     assertSame(expectedResult, result);
   }

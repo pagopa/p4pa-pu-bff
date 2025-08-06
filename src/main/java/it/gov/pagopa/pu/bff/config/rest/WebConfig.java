f8f4a57f3d773ce4e573f2d9410d159bedb1dc03
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-08-06 11:06:30 +0200

    P4PADEV-3629 added WebConfig.java

---
+++ src/main/java/it/gov/pagopa/pu/bff/config/rest/WebConfig.java
@@ -0,0 +1,14 @@
+package it.gov.pagopa.pu.bff.config.rest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(new StringToOffsetDateTimeConverter());
+  }
+}

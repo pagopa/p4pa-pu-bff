58dbfa5b9a63eb86b0d89f40866295c0c11bc57b
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-09-11 18:11:42 +0200

    P4ADEV-3685 updated generated openapi

--- src/test/java/it/gov/pagopa/pu/bff/mapper/OperatorDetailMapperTest.java
+++ src/test/java/it/gov/pagopa/pu/bff/mapper/OperatorDetailMapperTest.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.pu.bff.mapper;
 
 import it.gov.pagopa.pu.auth.dto.generated.OperatorDTO;
+import it.gov.pagopa.pu.bff.dto.generated.OperatorRole;
 import it.gov.pagopa.pu.bff.dto.generated.OperatorsDetail;
 import it.gov.pagopa.pu.bff.util.TestUtils;
 import it.gov.pagopa.pu.debtpositions.dto.generated.PagedModelDebtPositionTypeOrg;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 import uk.co.jemos.podam.api.PodamFactory;
+
+import java.util.List;
 
 class OperatorDetailMapperTest {
 
@@ -19,6 +22,7 @@ class OperatorDetailMapperTest {
     //given
     PagedModelDebtPositionTypeOrg pagedModelDebtPositionTypeOrg = podamFactory.manufacturePojo(PagedModelDebtPositionTypeOrg.class);
     OperatorDTO operatorDTO = podamFactory.manufacturePojo(OperatorDTO.class);
+    operatorDTO.setRoles(List.of("ROLE_ADMIN"));
 
     //when
     OperatorsDetail result = mapper.map(pagedModelDebtPositionTypeOrg, operatorDTO);
@@ -26,7 +30,7 @@ class OperatorDetailMapperTest {
     Assertions.assertNotNull(result);
     Assertions.assertEquals(operatorDTO.getOperatorId(), result.getOperatorId());
     Assertions.assertEquals(operatorDTO.getFiscalCode(), result.getOperatorFiscalCode());
-    Assertions.assertEquals(operatorDTO.getRoles(), result.getOperatorRole());
+    Assertions.assertEquals(OperatorRole.ROLE_ADMIN, result.getOperatorRole());
     Assertions.assertEquals(pagedModelDebtPositionTypeOrg.getEmbedded().getDebtPositionTypeOrgs(), result.getPagedOperatorsDetails().getContent());
     Assertions.assertEquals(pagedModelDebtPositionTypeOrg.getPage().getNumber(), result.getPagedOperatorsDetails().getNumber());
     Assertions.assertEquals(pagedModelDebtPositionTypeOrg.getPage().getTotalPages(), result.getPagedOperatorsDetails().getTotalPages());

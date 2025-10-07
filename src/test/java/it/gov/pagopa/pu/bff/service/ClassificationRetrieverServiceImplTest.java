98c3121a6ddfec4a748da6df3ed88a7761875152
michele.mazzini <michele.mazzini1996@gmail.com>
2025-10-07 14:35:59 +0200

    fix: increase code coverage pt. 2

--- src/test/java/it/gov/pagopa/pu/bff/service/ClassificationRetrieverServiceImplTest.java
+++ src/test/java/it/gov/pagopa/pu/bff/service/ClassificationRetrieverServiceImplTest.java
@@ -972,6 +972,30 @@ class ClassificationRetrieverServiceImplTest {
       verifyNoInteractions(classificationServiceMock);
     }
   }
+
+  @Test
+  void givenValidFiltersWhenGetClassificationsThenOk() {
+    long organizationId = 1L;
+    ClassificationFiltersDTO filters = new ClassificationFiltersDTO();
+    PageRequest pageable = PageRequest.of(0, 10);
+
+    UserInfo loggedUser = new UserInfo();
+    loggedUser.setUserId("user-123");
+
+    PagedModelClassification expected = new PagedModelClassification();
+    try (MockedStatic<AuthorizationService> authorizationMock = Mockito.mockStatic(AuthorizationService.class)) {
+      authorizationMock.when(() -> AuthorizationService.validateUserForOrganizationId(organizationId, loggedUser))
+        .thenAnswer(a -> null);
+
+      when(classificationServiceMock.getClassifications(organizationId, filters, pageable, accessToken))
+        .thenReturn(expected);
+
+      PagedModelClassification result = classificationRetrieverService.getClassifications(organizationId, filters, pageable, loggedUser, accessToken);
+
+      assertNotNull(result);
+      assertSame(expected, result);
+    }
+  }
 }
 
 

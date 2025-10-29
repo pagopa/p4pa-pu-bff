d02d87104f076b0545e18b2180c39e1a833f6b1c
EmVal <147710433+EmanueleValentini1@users.noreply.github.com>
2025-10-29 10:57:35 +0100

    feat: P4ADEV-4015 new receipt PDF (#311)

--- src/main/java/it/gov/pagopa/pu/bff/service/receipt/ReceiptFileServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/bff/service/receipt/ReceiptFileServiceImpl.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Locale;
@@ -24,14 +25,18 @@ public class ReceiptFileServiceImpl implements ReceiptFileService{
     public static final String RECEIPT_IUV = "iuv";
     public static final String RECEIPT_DEBTOR_NAME = "debtorName";
     public static final String RECEIPT_DEBTOR_FISCAL_CODE = "debtorFiscalCode";
-    public static final String RECEIPT_TOTAL_AMOUNT = "totalAmount";
     public static final String RECEIPT_PAYMENT_DATE = "paymentDate";
     public static final String RECEIPT_PSP_NAME = "pspName";
-    public static final String RECEIPT_FEE_AMOUNT = "feeAmount";
     public static final String RECEIPT_AMOUNT = "amount";
     public static final String RECEIPT_ORG_FISCAL_CODE = "orgFiscalCode";
-    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd MMMM yyyy, HH:mm:ss", Locale.ITALIAN);
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd MMMM yyyy, HH:mm", Locale.ITALIAN);
+    public static final DateTimeFormatter DATE_FORMATTER_FOOTER = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALIAN);
+    public static final DateTimeFormatter TIME_FORMATTER_FOOTER = DateTimeFormatter.ofPattern("HH:mm", Locale.ITALIAN);
     public static final String REMITTANCE_INFORMATION = "remittanceInformation";
+    public static final String IUD = "iud";
+    public static final String IUR = "iur";
+    public static final String EMISSION_DATE = "emissionDate";
+    public static final String EMISSION_TIME = "emissionTime";
 
     public ReceiptFileServiceImpl(DocumentComposition documentComposition) {
         this.documentComposition = documentComposition;
@@ -51,20 +56,20 @@ public class ReceiptFileServiceImpl implements ReceiptFileService{
     private Map<String, Object> buildTemplateModel(it.gov.pagopa.pu.debtpositions.dto.generated.ReceiptDetailDTO receiptDetail, Organization organization) {
         Map<String, Object> templateModel = new HashMap<>();
 
-        Long feeCents = receiptDetail.getFeeCents() != null ? receiptDetail.getFeeCents() : 0L;
-
         templateModel.put(RECEIPT_LOGO, StringUtils.defaultString(organization.getOrgLogo()));
         templateModel.put(RECEIPT_ORG_NAME,organization.getOrgName());
         templateModel.put(RECEIPT_IUV,StringUtils.defaultString(receiptDetail.getIuv()));
         templateModel.put(RECEIPT_DEBTOR_NAME,receiptDetail.getDebtor().getFullName());
         templateModel.put(RECEIPT_DEBTOR_FISCAL_CODE,receiptDetail.getDebtor().getFiscalCode());
-        templateModel.put(RECEIPT_TOTAL_AMOUNT, Utilities.formatPrice(receiptDetail.getPaymentAmountCents()+feeCents));
         templateModel.put(RECEIPT_PAYMENT_DATE,receiptDetail.getPaymentDateTime()!=null?receiptDetail.getPaymentDateTime().format(DATE_TIME_FORMATTER):"");
         templateModel.put(RECEIPT_PSP_NAME,receiptDetail.getPspCompanyName());
-        templateModel.put(RECEIPT_FEE_AMOUNT, Utilities.formatPrice(receiptDetail.getFeeCents()));
         templateModel.put(RECEIPT_AMOUNT, Utilities.formatPrice(receiptDetail.getPaymentAmountCents()));
         templateModel.put(RECEIPT_ORG_FISCAL_CODE,organization.getOrgFiscalCode());
         templateModel.put(REMITTANCE_INFORMATION,StringUtils.defaultString(receiptDetail.getRemittanceInformation()));
+        templateModel.put(IUR,receiptDetail.getIur());
+        templateModel.put(IUD,receiptDetail.getIud());
+        templateModel.put(EMISSION_DATE, LocalDateTime.now().format(DATE_FORMATTER_FOOTER));
+        templateModel.put(EMISSION_TIME, LocalDateTime.now().format(TIME_FORMATTER_FOOTER));
         return templateModel;
     }
 }

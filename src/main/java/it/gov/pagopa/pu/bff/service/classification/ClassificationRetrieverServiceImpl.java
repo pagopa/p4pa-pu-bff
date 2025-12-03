c083e25809d1b7a097397a85e2ef03c97e4ba9fc
michele.mazzini <michele.mazzini1996@gmail.com>
2025-12-03 13:01:26 +0100

    fix: remove unused import

--- src/main/java/it/gov/pagopa/pu/bff/service/classification/ClassificationRetrieverServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/bff/service/classification/ClassificationRetrieverServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 import java.util.*;
 import java.util.stream.Collectors;

2fd1669c8f8da2fab9cdc774c6392fe7b9aeea05
macacia <marco.cacia@eng.it>
2025-12-15 12:44:08 +0100

    feat: P4ADEV-4324 api debtpositiontypeorg detail add taxonomy code (#345)

--- src/main/java/it/gov/pagopa/pu/bff/mapper/DebtPositionTypeOrgDTOMapper.java
+++ src/main/java/it/gov/pagopa/pu/bff/mapper/DebtPositionTypeOrgDTOMapper.java
@@ -11,23 +11,26 @@ public interface DebtPositionTypeOrgDTOMapper {
 
   @Mapping(target = "debtPositionTypeDescription", source = "debtPositionTypeDescription")
   @Mapping(target = "debtPositionTypeCode", source = "debtPositionTypeCode")
+  @Mapping(target = "debtPositionTypeTaxonomyCode", source = "debtPositionTypeTaxonomyCode")
   @Mapping(target = "notifyOutcomePushOrgSilServiceApplicationName", source = "notifyOutcomePushOrgSilServiceApplicationName")
   @Mapping(target = "amountActualizationOrgSilServiceApplicationName", source = "amountActualizationOrgSilServiceApplicationName")
   @Mapping(target = "spontaneousFormCode", source = "spontaneousFormCode")
-  DebtPositionTypeOrgDTO map(DebtPositionTypeOrg debtPositionTypeOrg, String debtPositionTypeDescription, String debtPositionTypeCode,
+  DebtPositionTypeOrgDTO map(DebtPositionTypeOrg debtPositionTypeOrg, String debtPositionTypeDescription, String debtPositionTypeCode, String debtPositionTypeTaxonomyCode,
                              String notifyOutcomePushOrgSilServiceApplicationName, String amountActualizationOrgSilServiceApplicationName, String spontaneousFormCode);
 
   default DebtPositionTypeOrgDTO map(DebtPositionTypeOrg debtPositionTypeOrg, DebtPositionType debtPositionType,
                                      String notifyOutcomePushOrgSilServiceApplicationName, String amountActualizationOrgSilServiceApplicationName, String spontaneousFormCode) {
     String description = null;
     String code = null;
+    String taxonomyCode = null;
 
     if (debtPositionType != null) {
       description = debtPositionType.getDescription();
       code = debtPositionType.getCode();
+      taxonomyCode = debtPositionType.getTaxonomyCode();
     }
 
-    return map(debtPositionTypeOrg, description, code, notifyOutcomePushOrgSilServiceApplicationName, amountActualizationOrgSilServiceApplicationName, spontaneousFormCode);
+    return map(debtPositionTypeOrg, description, code, taxonomyCode, notifyOutcomePushOrgSilServiceApplicationName, amountActualizationOrgSilServiceApplicationName, spontaneousFormCode);
   }
 }
 

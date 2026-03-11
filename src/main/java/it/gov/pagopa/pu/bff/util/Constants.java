cf469073e1a8fc76ce3c961bb4f863415da6ac95
antonio.torre <antonio.torre@pagopa.it>
2026-03-11 12:51:53 +0100

    chore: Refactor to use DebtPositionOrigin centralized aggregated Set

--- src/main/java/it/gov/pagopa/pu/bff/util/Constants.java
+++ src/main/java/it/gov/pagopa/pu/bff/util/Constants.java
@@ -1,6 +1,9 @@
 package it.gov.pagopa.pu.bff.util;
 
+import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionOrigin;
+
 import java.time.ZoneId;
+import java.util.List;
 import java.util.TimeZone;
 
 public class Constants {
@@ -11,5 +14,12 @@ public class Constants {
   public static final TimeZone DEFAULT_TIMEZONE = TimeZone.getTimeZone(ZONEID);
 
   public static final String INSTALLMENT_REMITTANCE_INFORMATION_PLACEHOLDER = "Pagamento on-the-fly";
+
+  public static final List<DebtPositionOrigin> ORDINARY_DEBT_POSITION_ORIGINS = List.of(
+    DebtPositionOrigin.ORDINARY,
+    DebtPositionOrigin.ORDINARY_SIL,
+    DebtPositionOrigin.SPONTANEOUS,
+    DebtPositionOrigin.SPONTANEOUS_SIL,
+    DebtPositionOrigin.SPONTANEOUS_PSP);
 }
 

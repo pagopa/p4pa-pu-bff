206d15d5a8011cd08cd181898d1e8314c2bda27e
antonio.torre <antonio.torre@pagopa.it>
2026-04-28 18:08:58 +0200

    fix: Fix CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.0.5"
+  id("org.springframework.boot") version "4.0.6"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
   id("org.sonarqube") version "7.2.3.7755"
-  id("com.github.ben-manes.versions") version "0.53.0"
+  id("com.github.ben-manes.versions") version "0.54.0"
   id("org.openapi.generator") version "7.21.0"
   id("org.ajoberstar.grgit") version "5.3.2"
   id("com.gorylenko.gradle-git-properties") version "2.5.7"
@@ -50,22 +50,19 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "3.0.2"
+val springDocOpenApiVersion = "3.0.3"
 val janinoVersion = "3.1.12"
 val openApiToolsVersion = "0.2.10"
-val micrometerVersion = "1.6.4"
+val micrometerVersion = "1.6.5"
 val caffeineVersion = "3.2.3"
-val httpClientVersion = "5.6"
+val httpClientVersion = "5.6.1"
 val httpCoreVersion = "5.4.2"
 val mapStructVersion = "1.6.3"
 val commonsLang3Version = "3.20.0"
 
 val wiremockVersion = "3.13.2"
-val wiremockSpringBootVersion = "4.2.0"
+val wiremockSpringBootVersion = "4.2.1"
 val podamVersion = "8.0.2.RELEASE"
-
-// fix cve
-val jackson3CoreVersion = "3.1.1"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
@@ -90,9 +87,6 @@ dependencies {
   implementation("org.apache.httpcomponents.core5:httpcore5:$httpCoreVersion")
   implementation("org.mapstruct:mapstruct:${mapStructVersion}")
 
-
-  // CVE fix
-  implementation("tools.jackson.core:jackson-core:$jackson3CoreVersion")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")

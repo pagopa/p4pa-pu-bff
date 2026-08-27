07d206f3781bb132c0d09ba08520e03920ff4e3d
antocalo <antonio.calo@nttdata.com>
2026-08-27 13:08:53 +0200

    P4ADEV-4964 add tests

---
+++ src/test/java/it/gov/pagopa/pu/bff/service/pdnd_service/PdndServiceRetrieverImplTest.java
@@ -0,0 +1,59 @@
+package it.gov.pagopa.pu.bff.service.pdnd_service;
+
+import it.gov.pagopa.pu.auth.dto.generated.UserInfo;
+import it.gov.pagopa.pu.bff.connector.organization.PdndServiceService;
+import it.gov.pagopa.pu.bff.service.AuthorizationService;
+import it.gov.pagopa.pu.bff.util.TestUtils;
+import it.gov.pagopa.pu.organization.dto.generated.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PdndServiceRetrieverImplTest {
+
+  private static final Long ORGANIZATION_ID = 123L;
+  private static final String SUB_UNIT_CODE = "SUB_UNIT_001";
+  private static final String ACCESS_TOKEN = "fakeAccessToken";
+
+  private static final UserInfo USER_INFO = TestUtils.getPodamFactory().manufacturePojo(UserInfo.class);
+
+  @Mock
+  private AuthorizationService authorizationServiceMock;
+  @Mock
+  private PdndServiceService pdndServiceServiceMock;
+  @InjectMocks
+  private PdndServiceRetrieverImpl pdndServiceRetriever;
+
+  @AfterEach
+  void tearDown() {
+    verifyNoMoreInteractions(
+      authorizationServiceMock,
+      pdndServiceServiceMock
+    );
+  }
+
+  @Test
+  void givenAuthorizedAdminWhenCreatePdndServiceThenReturnService() {
+    PdndServiceRequestDTO pdndServiceRequestDTO = TestUtils.getPodamFactory().manufacturePojo(PdndServiceRequestDTO.class);
+    PdndService expectedResult = TestUtils.getPodamFactory().manufacturePojo(PdndService.class);
+
+    doNothing().when(authorizationServiceMock)
+      .validateAdminRole(ORGANIZATION_ID, USER_INFO);
+
+    when(pdndServiceServiceMock.savePdndService(ORGANIZATION_ID, pdndServiceRequestDTO, SUB_UNIT_CODE, ACCESS_TOKEN))
+      .thenReturn(expectedResult);
+
+    PdndService result = pdndServiceRetriever.createPdndService(ORGANIZATION_ID, pdndServiceRequestDTO, SUB_UNIT_CODE, USER_INFO, ACCESS_TOKEN);
+
+    assertSame(expectedResult, result);
+  }
+
+}
